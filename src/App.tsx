d7bf41c3512af4810f51cc915fa74b9ef3da03d5
CommitTODev <abhishekshivale45@gmail.com>
2024-04-05 22:12:28 +0530

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Navbar from "./components/other/Navbar";
 import Hero from "./components/other/Hero";
 import Partnner from "./components/other/Patnner";
--- src/components/other/Patnner.tsx
+++ src/components/other/Patnner.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import partner from "@/assets/Partnner.png";
 function Patnner() {
   return (
